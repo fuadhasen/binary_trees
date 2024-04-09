872ee868682b61f93ad22a21a0c3a4af7f303a57
fuadhasen <fuadhas6634@gmail.com>
2024-04-09 07:10:29 -0700

    Insert_left update

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,15 +12,10 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *temp = NULL;
-	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new = binary_tree_node(parent, value);
 
 	if (new == NULL || parent == NULL)
 		return (NULL);
-
-	new->n = value;
-	new->parent = parent;
-	new->right = NULL;
-	new->left = NULL;
 
 	if (parent->left != NULL)
 	{
