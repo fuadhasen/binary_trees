3d8970660fe48b32daeb3d420142062f1639fa64
fuadhasen <fuadhas6634@gmail.com>
2024-04-12 16:37:48 +0530

    Bt-balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,20 +1,23 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - to measure the height of the Bt-height
- * 		by calculating height of left and right subtree
- * 		separately. then their difference is Bf.
+ * binary_tree_balance - to measure the height of the Bt-height
+ * by calculating height of left and right subtree
+ * separately. then their difference is Bf.
  * @tree: pointer to the root node
  *
- * Return: Height of the binary tree
+ * Return: balance factor of the binary tree
  */
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left, right;
+
 	if (tree == NULL)
 		return (0);
-
-	return (bt_height(tree->left) - bt_height(tree->right));
+	left = bt_height(tree->left);
+	right = bt_height(tree->right);
+	return (left - right);
 }
 
 /**
@@ -30,13 +33,13 @@ size_t bt_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
- 
+
 	if (tree->left == NULL && tree->right == NULL)
-		return (0);
- 
+		return (1);
+
 	left_ht = bt_height(tree->left);
 	right_ht = bt_height(tree->right);
- 
+
 	if (left_ht > right_ht)
 		return (left_ht + 1);
 	else
